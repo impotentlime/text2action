661417372b671bdb6ca7cf23da01d17b08b39469
lucasjsyou <lucasjsyou@gmail.com>
2018-02-20 21:33:34 +0900

    Removed filebrowser.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Post
-from mce_filebrowser.admin import MCEFilebrowserAdmin
+# from mce_filebrowser.admin import MCEFilebrowserAdmin
 # Register your models here.
 
-class MyModelAdmin(MCEFilebrowserAdmin):
-    pass
-
-admin.site.register(Post, MyModelAdmin)
+admin.site.register(Post)
